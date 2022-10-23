b720afc0d07b1d7fc80fe2b0e9f9aa6a0e930848
juvcarl <juvcarl@gmail.com>
2022-10-22 21:43:00 -0400

    update app icon and change version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ plugins {
 android {
     defaultConfig {
         applicationId = "com.juvcarl.shoplist"
-        versionCode = 1
-        versionName = "0.0.1" // X.Y.Z; X = Major, Y = minor, Z = Patch level
+        versionCode = 2
+        versionName = "0.0.2" // X.Y.Z; X = Major, Y = minor, Z = Patch level
 
         // Custom test runner to set up Hilt dependency graph
         testInstrumentationRunner = "com.juvcarl.shoplist.testing.ShopListTestRunner"
